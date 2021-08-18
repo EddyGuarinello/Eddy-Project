4b566547e3bc3ecc1b8c2564c388ef0de44fa5ae
EddyGuarinello <87097263+EddyGuarinello@users.noreply.github.com>
2021-08-18 10:51:07 -0300

    Fixes
    1

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ buttonFooter.addEventListener("click", function(){
     return location.href = '#section1';
 })
 
+let toggle = document.querySelector(".foto");
+let toggleb = document.querySelector("#pixel");
+ 
+toggleb.classList.toggle("pixel");
+toggle.addEventListener("click", function(){
+    toggleb.classList.toggle("pixel");
+})
+toggleb.addEventListener("click", function(){
+    toggleb.classList.toggle("pixel");
+})
