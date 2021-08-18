930e86f3bb13e9997c1c46c903bce8119652a16b
EddyGuarinello <87097263+EddyGuarinello@users.noreply.github.com>
2021-08-17 22:33:26 -0300

    contato temp-fix

--- script.js
+++ script.js
@@ -3,8 +3,3 @@ buttonFooter.addEventListener("click", function(){
     return location.href = '#section1';
 })
 
-const buttoncontato = document.querySelector("#buttoncontato");
-buttoncontato.addEventListener("click", function(){
-    prompt("Enviado com Sucesso")
-    return location.href = 'https://eddyguarinello.github.io/Eddy-Project/index.html';
-})
