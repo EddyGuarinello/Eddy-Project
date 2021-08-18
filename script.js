510608dc40cdce464c790dbedb8d275831022640
EddyGuarinello <87097263+EddyGuarinello@users.noreply.github.com>
2021-08-18 11:24:41 -0300

    final fix toggle

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ buttonFooter.addEventListener("click", function(){
 
 let toggle = document.querySelector(".foto");
 let toggleb = document.querySelector("#pixel");
- 
+
 toggleb.classList.toggle("pixel");
+
 toggle.addEventListener("click", function(){
-    toggleb.classList.toggle("pixel");
+   toggle.classList.remove("foto");
+   toggle.classList.add("toggle");
+   toggleb.classList.remove("pixel");
 })
 toggleb.addEventListener("click", function(){
-    toggleb.classList.toggle("pixel");
+    toggleb.classList.add("pixel");
+    toggle.classList.remove("toggle");
+    toggle.classList.add("foto");
 })
