9a5cb0f2504d498ea39f098ed584123bcca42912
EddyGuarinello <87097263+EddyGuarinello@users.noreply.github.com>
2021-08-18 01:04:27 -0300

    test pixel art

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ buttonFooter.addEventListener("click", function(){
     return location.href = '#section1';
 })
 
+let toogle = document.querySelector("#foto");
+toogle.addEventListener("click", function(){
+    if (toogle.src == "https://eddyguarinello.github.io/Eddy-Project/foto%20eddy2.jpg"){
+       toogle.src = "https://eddyguarinello.github.io/Eddy-Project/eddy%20pixel.png";
+    }else{
+       toogle.src = "https://eddyguarinello.github.io/Eddy-Project/foto%20eddy2.jpg";
+    }
+})
