947d2ec283ad580812eb39160250576b6e9883d6
EddyGuarinello <87097263+EddyGuarinello@users.noreply.github.com>
2021-08-17 22:10:47 -0300

    contato minor improvments attempt

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const buttonFooter = document.querySelector("#buttonFooter");
 buttonFooter.addEventListener("click", function(){
     return location.href = '#section1';
 })
+
+const buttoncontato = document.querySelector("#buttoncontato");
+buttoncontato.addEventListener("click", function(){
+    prompt("Enviado com Sucesso")
+    return location.href = 'https://eddyguarinello.github.io/Eddy-Project/index.html';
+})
